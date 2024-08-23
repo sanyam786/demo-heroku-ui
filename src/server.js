b2659a0d38e8185802567ecdd4d103e64ac81def
sanyam786 <38782200+sanyam786@users.noreply.github.com>
2024-08-23 12:41:31 +0530

    procfile moved

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-16-crud'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-16-crud/index.html'));});
+'/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
