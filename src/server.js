21e7f9d4f4e18b4ca23cb95297e97ca849aea2d9
sanyam786 <38782200+sanyam786@users.noreply.github.com>
2024-08-23 13:37:05 +0530

    updated path

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-16-crud'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-16-crud/index.html'));});
+'/dist/angular-16-crud/src/index.html'));});
 app.listen(process.env.PORT || 8080);
