33bb6a11b00dc1ff9463ddc7ceb6cb99b25af7ee
sanyam786 <38782200+sanyam786@users.noreply.github.com>
2024-08-23 14:47:13 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-16-crud'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/index.html'));});
