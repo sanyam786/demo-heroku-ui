cfc04f667f175d6328c7fc6b7c579ee51c32c55f
sanyam786 <38782200+sanyam786@users.noreply.github.com>
2024-09-15 01:14:41 +0530

    fix for scroll height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, HostListener } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sadhumargi Sangh Directory';
+ 
+  constructor() {
+    this.setViewportHeight();
+    this.addEventListeners();
+  }
+
+  // Function to dynamically calculate and set the viewport height
+  setViewportHeight(): void {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  // Function to listen for resize and load events
+  addEventListeners(): void {
+    window.addEventListener('resize', () => this.setViewportHeight());
+    window.addEventListener('load', () => this.setViewportHeight());
+  }
 }
